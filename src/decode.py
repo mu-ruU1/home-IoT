50c23e113e08dea30290464c322a22d5651163a9
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-12-16 22:34:58 +0900

    フォーマット判定まで

--- src/decode.py
+++ src/decode.py
@@ -1,1 +1,59 @@
 import json
+import sys
+import statistics
+
+T_NEC = 562
+T_AEHA = 425
+T_SONY = 600
+
+
+def calc_period(value):
+    res = []
+    median = 0  # 中央値
+
+    # 信号ON時のデータを取得
+    for i in range(len(value)):
+        if (i % 2 == 0) and (value[i] < 1000):
+            res.append(value[i])
+
+    median = statistics.median(value)
+    print("T (変調単位): ", median)
+
+    # 通信フォーマットの判別
+    nec = abs(median - T_NEC)
+    aeha = abs(median - T_AEHA)
+    sony = abs(median - T_SONY)
+
+    format = [nec, aeha, sony]
+
+    index = format.index(min(format))
+
+    if index == 0:
+        print("NECフォーマット\nこのプログラムは対応していません")
+        exit(0)
+    elif index == 1:
+        print("AEHAフォーマット\nデコードを行います\n")
+    elif index == 2:
+        print("SONYフォーマット\nこのプログラムは対応していません")
+        exit(0)
+
+
+def main(path, key):
+    with open(path, "r") as f:
+        values = json.load(f)
+        value = values[key]
+
+    calc_period(value)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    if len(args) == 3:
+        path = args[1]
+        key = args[2]
+        main(path, key)
+    else:
+        print("error: pathとkeyを指定して下さい")
+        print("$ decode.py <json_path> <json_key>")
+        sys.exit(1)
